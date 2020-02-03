e6dc5ed183680bd6f0483e77085065a17c7e4c78
Simyan <simyan10@gmail.com>
2020-02-03 14:39:32 +0400

    setup a basic mongodb connection

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import mongoose from 'mongoose';
+require('dotenv').config();
 
 
 const app = express();
@@ -7,4 +9,12 @@ app.get('/', (req, res) => {
     res.send('Hey there!');
 });
 
-app.listen(6101, () => console.log("This is the server"));
+
+
+mongoose.connect(    
+    `${process.env.DB_CONNECTION}`,  { useNewUrlParser: true }).then(
+    () => {app.listen(6101, () => console.log("This is the server"));})
+    .catch(
+        err =>{console.log("Error in connecting to Mongo DB:" + err);
+        });
+
